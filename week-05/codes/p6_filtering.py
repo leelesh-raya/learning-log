1641dff0ce52f86e5f1895203b1cf08e4438a4a9
Leelesh Raya <leeleshrv@gmail.com>
2026-05-27 18:56:02 +0530

    Add week 5 day 2 — filtering, aggregation, data cleaning and updated notes

--- week-05/codes/p6_filtering.py
+++ week-05/codes/p6_filtering.py
@@ -1,13 +1,19 @@
 import pandas as pd, numpy as np
 
+# Filtering = Keeping rows or columns that match a condition
 df = pd.read_csv("pokemon.csv")
 
-psychic = df.loc[df['Weight']>=90 , ['Name','Weight']]
-water_pokemon = df[(df['Type1']=='Water') | (df['Type2']=='Water')  ]
-ff_pokemon = df[(df['Type1']=='Fire') &
-                (df['Type2']=='Flying')]
 
+heavy = df.loc[df["Weight"] >= 90, ["Name", "Weight"]] # Use loc when dealing with index or more than one argument
 
-print(psychic)
+# Keeping rows that match either of the conditions
+water_pokemon = df[(df["Type1"] == "Water") | (df["Type2"] == "Water")]
+
+# Kepping rows that match both conditions
+ff_pokemon = df[(df["Type1"] == "Fire") & (df["Type2"] == "Flying")]
+  
+
+print(heavy)
 print(water_pokemon)
 print(ff_pokemon)
+
--- week-05/codes/p7_aggregate.py
+++ week-05/codes/p7_aggregate.py
@@ -3,6 +3,7 @@ import pandas as pd
 # Aggregate functions = Used to summarize and analyze data
 
 df = pd.read_csv('pokemon.csv')
+
 # Calculate mean for whole df
 print(df.mean(numeric_only=True)) # This argument prevents error by dropping non-numerical columns
 
@@ -13,10 +14,8 @@ print(df['Height'].max())
 print(df['Height'].min())
 print(df['Height'].count())
 
-
 # What if we have to group rows with similar properties and apply calculations on these different groups?
-group = df.groupby('Type1') # this will make groups pokemons with similar type1
-
+group = df.groupby('Type1') # this will make groups of pokemons with similar type1
 
 # Calculating metrics per group
 print(group['Height'].mean()) # Calculate avg height for each group
@@ -24,3 +23,4 @@ print(group['Height'].sum())
 print(group['Height'].max())
 print(group['Height'].min())
 print(group['Height'].count())
+
